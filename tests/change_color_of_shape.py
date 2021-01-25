fb81e050a3b3228a8e1dbb710dc3c0e4d1558f46
putzw <wilhelm.putz@kapsch.net>
2021-01-25 08:21:46 +0100

    update tests

--- tests/change_color_of_shape.py
+++ tests/change_color_of_shape.py
@@ -1,11 +1,23 @@
+import sys
+sys.path.insert(0, "../")
 from drawioedit import DrawIOEdit
 import os
-
+red='#ff0000'
 base_path=os.path.dirname(os.path.realpath(__file__))
+drawing=DrawIOEdit(file_path=f'{base_path}/input.drawio.svg')
+drawing.set_shape_color('DC1-SW1',red)
+drawing.set_shape_color('bbnew-1',red)
+drawing.set_shape_color('iperf-1',red)
+drawing.set_shape_color('iperf-3',red)
 
-drawing=DrawIOEdit(file_path=f'{base_path}/test.drawio.png')
-drawing.set_shape_style('R1','fillColor','#ff0000')
-drawing.set_shape_style('R2','fillColor','#0000ff')
-drawing.set_shape_style('not existing','fillColor','#0000ff')
+
+
+for link in drawing.find_link_between_nodes('DC1-SW1','bbnew-1'):
+    link.attrib['style']=drawing._edit_style(link.attrib['style'],'strokeColor','#ff0000')
+
+for link in drawing.find_link_between_nodes('Backbone-1','BB-new'):
+    link.attrib['style']=drawing._edit_style(link.attrib['style'],'strokeColor','#ff0000')
+
+
 
 print(drawing.xml())

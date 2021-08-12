f7e39b4c7dc944a8d99c16d0562aa1af9c59c4a8
putzw <wilhelm.putz@kapsch.net>
2021-08-12 11:02:51 +0200

    add xvfb auto display selection

--- drawioedit/__init__.py
+++ drawioedit/__init__.py
@@ -229,7 +229,7 @@ class DrawIOEdit(object):
             tmp_fd,tmp_path=tempfile.mkstemp(suffix='.drawio')
             with os.fdopen(tmp_fd,'w') as tmp_fh:
                 tmp_fh.write(self.xml())
-            result=subprocess.check_output([self._xvfb_run_path,self._draw_io_path,"-x","-e","-o",destination_file,tmp_path],universal_newlines=True)
+            result=subprocess.check_output([self._xvfb_run_path,"-a",self._draw_io_path,"-x","-e","-o",destination_file,tmp_path],universal_newlines=True)
             self._log.debug(result)
             os.unlink(tmp_path)
         else:
